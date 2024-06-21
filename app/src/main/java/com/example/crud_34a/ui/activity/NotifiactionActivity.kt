c0079b857f81307e537f77e0d15e80b54a603173
AakashShahi <129369974+AakashShahi@users.noreply.github.com>
2024-06-21 11:56:48 +0545

    Notification Changes

--- app/src/main/java/com/example/crud_34a/ui/activity/NotifiactionActivity.kt
+++ app/src/main/java/com/example/crud_34a/ui/activity/NotifiactionActivity.kt
@@ -1,13 +1,17 @@
 package com.example.crud_34a.ui.activity
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Build.VERSION_CODES.O
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.crud_34a.R
@@ -37,12 +41,22 @@ class NotifiactionActivity : AppCompatActivity() {
             builder.setSmallIcon(R.drawable.baseline_add_24)
             builder.setContentTitle("My Notification")
             builder.setContentText("This is my notification")
-            manager.notify(1,builder.build())
+
         }
         else{
             builder.setSmallIcon(R.drawable.baseline_add_24)
             builder.setContentTitle("My Notification")
             builder.setContentText("This is my notification")
         }
+
+        var notificationManagerCompat= NotificationManagerCompat.from(this@NotifiactionActivity)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        notificationManagerCompat.notify(1,builder.build())
     }
 }
